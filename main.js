0ece868f10e15c855f4df562e2ced4d29ddf44f2
Плотников Евгений Анатольевич <plotnikovea@altarix.ru>
2019-09-16 23:34:00 +0400

    ertet

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 100);
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(20, 5, 30);
 
 var scene = new THREE.Scene();
@@ -18,60 +18,50 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 // controls.minPolarAngle = Math.PI / 2; //раскомментировать для ограничения
 // controls.maxPolarAngle = Math.PI / 2;
 
+
+
 var geometry = new THREE.BoxGeometry(2,1,1);
 var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial());
 scene.add(mesh);
-scene.add(mesh);
 
-var plane = new THREE.GridHelper(20, 40);
-scene.add(plane);
-
-
+// var plane = new THREE.GridHelper(20, 40);
+// scene.add(plane);
+//
 
 
 
 
 
-
-function CustomSinCurve( scale ) {
-
-  THREE.Curve.call( this );
-
-  this.scale = ( scale === undefined ) ? 1 : scale;
-
-}
-
-CustomSinCurve.prototype = Object.create( THREE.Curve.prototype );
-CustomSinCurve.prototype.constructor = CustomSinCurve;
-
-CustomSinCurve.prototype.getPoint = function ( t ) {
-
-  var tx = t * 8 - 1.5;
-  var ty = Math.sin( 2 * Math.PI * t );
-  var tz = 0;
-
-  return new THREE.Vector3( tx, ty, tz ).multiplyScalar( this.scale );
-
-};
 
 const curve = new THREE.SplineCurve3([
   new THREE.Vector3(0,0,0),
-  new THREE.Vector3(0,-1,0),
-  new THREE.Vector3(0,-4,2),
-  new THREE.Vector3(0,-10,1),
-  new THREE.Vector3(2,-15,0),
-  new THREE.Vector3(0,-20,0),
+  new THREE.Vector3(0,-10,0),
+  new THREE.Vector3(0,-40,20),
+  new THREE.Vector3(0,-100,10),
+  new THREE.Vector3(2,-150,0),
+  new THREE.Vector3(0,-200,0),
 ]);
 
+const loader = new THREE.TextureLoader();
+const material = new THREE.MeshBasicMaterial({
+  map: loader.load('2.png'),
+});
 
-var path = new CustomSinCurve( 10 );
-var geometry = new THREE.TubeGeometry( curve, 100, 0.1, 19, false );
-var material = new THREE.MeshBasicMaterial( { color: 0x000fff } );
+const width = 30;
+const height = 42;
+const geometrye = new THREE.BoxGeometry(width,height,0.1);
+var meshe = new THREE.Mesh( geometrye, material );
+
+meshe.rotation.x = 4.677;
+meshe.position.z = -2.5;
+
+scene.add( meshe);
+
+console.log(meshe);
+var geometry = new THREE.TubeGeometry( curve, 100, 0.2, 19, false );
+// var material = new THREE.MeshBasicMaterial( { color: 0x000fff } );
 var mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
-
-
-
 
 
 
@@ -81,6 +71,8 @@ function animate() {
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
+  // meshe.rotation.x += 0.001;
+  // console.log(meshe.rotation.x)
 }
 
 animate();
